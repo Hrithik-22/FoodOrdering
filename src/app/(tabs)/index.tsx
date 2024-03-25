cd7221bad4956f8b4206d77a73c24c4534561c57
Hrithik Kedare <kedarehrithik@gmail.com>
2024-03-25 16:35:29 +0530

    first-commit

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,8 @@
 import { View } from "react-native";
 import products from "@/assets/data/products";
-import ProductListItem from "@/src/components/ProductListItem";
+// import ProductListItem from "@/src/components/ProductListItem";
 // const product = products[0];
 
 export default function MenuScreen() {
-  return (
-    <View>
-      <ProductListItem product={products[0]} />
-    </View>
-  );
+  return <View>{/* <ProductListItem product={products[0]} /> */}</View>;
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,14 +1,14 @@
-import { Text, View, Image } from "react-native";
+// import { Text, View, Image } from "react-native";
 
-const ProductListItem = ({ product }) => {
-  return (
-    <View className=" bg-white p-10 rounded-2xl">
-      {/* aspect-ratio :1/1 */}
-      <Image source={{ uri: product.image }} className="aspect-square w-full" />
-      <Text className="font-semibold text-xl my-10 ">{product.name}</Text>
-      <Text className="text-tintColorLight font-bold">${product.price}</Text>
-    </View>
-  );
-};
+// const ProductListItem = ({ product }) => {
+//   return (
+//     <View className=" bg-white p-10 rounded-2xl">
+//       {/* aspect-ratio :1/1 */}
+//       <Image source={{ uri: product.image }} className="aspect-square w-full" />
+//       <Text className="font-semibold text-xl my-10 ">{product.name}</Text>
+//       <Text className="text-tintColorLight font-bold">${product.price}</Text>
+//     </View>
+//   );
+// };
 
-export default ProductListItem;
+// export default ProductListItem;
