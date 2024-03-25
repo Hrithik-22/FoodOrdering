88be251436b70a43572b144e7d3fabbff147c81d
Hrithik Kedare <kedarehrithik@gmail.com>
2024-03-25 16:32:06 +0530

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import products from "@/assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
+// const product = products[0];
+
+export default function MenuScreen() {
+  return (
+    <View>
+      <ProductListItem product={products[0]} />
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "regal-blue": "#243c5a",
+        tintColorLight: "#2f95dc",
+      },
+    },
+  },
+  plugins: [],
+};
