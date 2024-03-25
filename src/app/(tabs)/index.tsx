5ed96d49df87136335801f9f0954c52fdd35465c
Hrithik Kedare <kedarehrithik@gmail.com>
2024-03-25 20:24:58 +0530

    added-flatlist

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,21 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import products from "@/assets/data/products";
-// import ProductListItem from "@/src/components/ProductListItem";
+import ProductListItem from "@/src/components/ProductListItem";
 // const product = products[0];
 
 export default function MenuScreen() {
-  return <View>{/* <ProductListItem product={products[0]} /> */}</View>;
+  return (
+    // <View>
+    //   <ProductListItem product={products[0]} />
+    // </View>
+    <View>
+      <FlatList
+        data={products}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+      />
+    </View>
+  );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,14 +1,26 @@
-// import { Text, View, Image } from "react-native";
+import { Text, View, Image } from "react-native";
+import { Product } from "../types";
 
-// const ProductListItem = ({ product }) => {
-//   return (
-//     <View className=" bg-white p-10 rounded-2xl">
-//       {/* aspect-ratio :1/1 */}
-//       <Image source={{ uri: product.image }} className="aspect-square w-full" />
-//       <Text className="font-semibold text-xl my-10 ">{product.name}</Text>
-//       <Text className="text-tintColorLight font-bold">${product.price}</Text>
-//     </View>
-//   );
-// };
+export const DefaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
-// export default ProductListItem;
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View className=" bg-white p-4 rounded-2xl flex-1 max-w-[50%]">
+      {/* aspect-ratio :1/1 */}
+      <Image
+        resizeMode="contain"
+        source={{ uri: product.image || DefaultPizzaImage }}
+        className="aspect-square  max-w-full"
+      />
+      <Text className="font-semibold text-xl my-10 ">{product.name}</Text>
+      <Text className="text-tintColorLight font-bold">${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
