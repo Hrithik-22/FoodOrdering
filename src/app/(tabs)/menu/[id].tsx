4b3bde6ed6ced5815167bf71c5d4a9cdd5da1c90
Hrithik Kedare <kedarehrithik@gmail.com>
2024-04-04 21:10:10 +0530

    Product Details Screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,13 +1,54 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, Pressable } from "react-native";
+import React, { useState } from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@/assets/data/products";
+import { DefaultPizzaImage } from "@/src/components/ProductListItem";
+import Button from "@/src/components/Button";
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
+  const [selectedSize, setSelectedSize] = useState("M");
   const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id.toString() === id);
+  const addToCart = () => {
+    console.warn("Adding product  with size:", selectedSize);
+  };
+  // if product is not found(undefined)
+  if (!product) {
+    return <Text>Product not Found</Text>;
+  }
   return (
-    <View className="bg-yellow-400">
-      <Stack.Screen options={{ title: `Details:${id}` }} />
-      <Text className="text-xl bg-red-600">ProductDetailsScreen : {id}</Text>
+    <View className="bg-white flex-1 p-3">
+      <Stack.Screen options={{ title: `${product.name}` }} />
+      <Image
+        className="aspect-square  max-w-full"
+        resizeMode="contain"
+        source={{ uri: product.image || DefaultPizzaImage }}
+      />
+      <Text>Select Size</Text>
+      <View className="flex-row justify-around my-2">
+        {sizes.map((size) => (
+          <Pressable
+            onPress={() => {
+              setSelectedSize(size);
+            }}
+            key={size}
+            className={`${
+              selectedSize === size ? "bg-slate-200" : "bg-white"
+            }  w-12 aspect-square rounded-3xl justify-center items-center `}
+          >
+            <Text
+              className={`${
+                selectedSize === size ? "text-black" : "text-slate-400"
+              } text-xl font-semibold`}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+      <Text className="text-xl font-bold mt-auto">${product.price}</Text>
+      <Button text="Add to Cart" onPress={addToCart} />
     </View>
   );
 };
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,7 +2,7 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {
   DarkTheme,
   DefaultTheme,
-  ThemeProvider
+  ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
@@ -14,17 +14,17 @@ import { NativeWindStyleSheet } from "nativewind";
 import "../../global.css";
 
 NativeWindStyleSheet.setOutput({
-  default: "native"
+  default: "native",
 });
 
 export {
   // Catch any errors thrown by the Layout component.
-  ErrorBoundary
+  ErrorBoundary,
 } from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)"
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -33,7 +33,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
-    ...FontAwesome.font
+    ...FontAwesome.font,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -58,7 +58,7 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "light" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
