b7198ddaf7a0c0f9af1196239959f10d1562b549
Hrithik Kedare <kedarehrithik@gmail.com>
2024-04-16 21:51:03 +0530

    Additional features

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Platform, View, FlatList } from "react-native";
+import { Platform, View, FlatList, Text } from "react-native";
 import { useCart } from "@/src/providers/CartProvider";
 import CartListItem from "../components/CartListItem";
+import Button from "../components/Button";
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
   return (
-    <View>
+    <View className="p-10">
       {/* <Text>CartScreen length: {items.length}</Text> */}
       <FlatList
         data={items}
-        contentContainerStyle={{ padding: 10, gap: 10 }}
+        contentContainerStyle={{ gap: 10 }}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
       />
+      <Text className="mt-5 text-xl font-bold">Total: ${total}</Text>
+      <Button text="Checkout" />
       <StatusBar style={Platform.OS === "android" ? "light" : "auto"} />
     </View>
   );
