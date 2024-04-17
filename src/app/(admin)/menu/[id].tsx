0a2b3386da1f28f3f52760eea570251bbf493277
Hrithik Kedare <kedarehrithik@gmail.com>
2024-04-17 11:43:17 +0530

    Admin side

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,45 @@
+import { View, Text, Image, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@/assets/data/products";
+import { DefaultPizzaImage } from "@/src/components/ProductListItem";
+import Button from "@/src/components/Button";
+import { useCart } from "@/src/providers/CartProvider";
+import { PizzaSize } from "@/src/types";
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailsScreen = () => {
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+  const { id } = useLocalSearchParams();
+  const router = useRouter();
+  const { addItem } = useCart();
+  const product = products.find((p) => p.id.toString() === id);
+  const addToCart = () => {
+    if (!product) {
+      return <Text>Product not Found</Text>;
+    }
+    // console.warn("Adding product  with size:", selectedSize);
+    addItem(product, selectedSize);
+    router.push("/cart");
+  };
+  // if product is not found(undefined)
+  if (!product) {
+    return <Text>Product not Found</Text>;
+  }
+  return (
+    <View className="bg-white flex-1 p-3">
+      <Stack.Screen options={{ title: `${product.name}` }} />
+      <Image
+        className="aspect-square  max-w-full"
+        resizeMode="contain"
+        source={{ uri: product.image || DefaultPizzaImage }}
+      />
+
+      <Text className="text-2xl font-bold ">${product.name}</Text>
+
+      <Text className="text-xl  font-medium ">${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductDetailsScreen;
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,6 @@
 import { Text, Image, Pressable } from "react-native";
 import { Product } from "../types";
-import { Link } from "expo-router";
+import { Link, useSegments } from "expo-router";
 
 export const DefaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
@@ -10,8 +10,9 @@ type ProductListItemProps = {
 };
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
+  const segments=useSegments();
   return (
-    <Link href={`/menu/${product.id}`} asChild>
+    <Link href={`/${segments[0]}/menu/${product.id}`} asChild>
       <Pressable className=" bg-white p-4 rounded-2xl flex-1 max-w-[50%]">
         {/* aspect-ratio :1/1 */}
         <Image
