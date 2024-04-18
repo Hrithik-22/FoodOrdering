ad9c311ddebfd1f8384141bfcb3588b1f193ab1a
Hrithik Kedare <kedarehrithik@gmail.com>
2024-04-18 20:03:31 +0530

    Update and Delete Product

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -6,6 +6,9 @@ import { DefaultPizzaImage } from "@/src/components/ProductListItem";
 import Button from "@/src/components/Button";
 import { useCart } from "@/src/providers/CartProvider";
 import { PizzaSize } from "@/src/types";
+import { Link } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "@/src/constants/Colors";
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
@@ -28,6 +31,25 @@ const ProductDetailsScreen = () => {
   }
   return (
     <View className="bg-white flex-1 p-3">
+      <Stack.Screen
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href={`/(admin)/menu/create?id=${id}`} asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
       <Stack.Screen options={{ title: `${product.name}` }} />
       <Image
         className="aspect-square  max-w-full"
--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,45 +1,74 @@
-import { View, Text,TextInput,Image } from 'react-native'
-import React, { useState } from 'react'
-import { Stack } from 'expo-router';
-import Button from '@/src/components/Button'
-import { DefaultPizzaImage } from '@/src/components/ProductListItem';
-import Colors from '@/src/constants/Colors';
-import * as ImagePicker from 'expo-image-picker';
+import { View, Text, TextInput, Image, Alert } from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
+import Button from "@/src/components/Button";
+import { DefaultPizzaImage } from "@/src/components/ProductListItem";
+import Colors from "@/src/constants/Colors";
+import * as ImagePicker from "expo-image-picker";
 const createProductScreen = () => {
   const [image, setImage] = useState<string | null>(null);
-  const [name,setName]=useState('');
-  const [price,setPrice]=useState('');
-  const [errors,setErrors]=useState('');
-  const resetFields=()=>{
-    setName('');
-    setPrice('');
-  }
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [errors, setErrors] = useState("");
+  const { id } = useLocalSearchParams();
+  const isUpdating = !!id;
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
 
-  const validateInput=()=>{
-    setErrors('');
-    if(!name){
-      setErrors('Name is required');
+  const validateInput = () => {
+    setErrors("");
+    if (!name) {
+      setErrors("Name is required");
       return false;
     }
-    if(!price){
-      setErrors('Price is required');
+    if (!price) {
+      setErrors("Price is required");
       return false;
     }
-    if(isNaN(parseFloat(price))){
-      setErrors('Price is not a Number');
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price is not a Number");
       return false;
     }
     return true;
   };
-  const onCreate=()=>{
-    if(!validateInput()){
+
+  const onSubmit = () => {
+    if (isUpdating) {
+      onUpdate();
+    } else {
+      onCreate();
+    }
+  };
+  const onUpdate = () => {
+    if (!validateInput()) {
+      return;
+    }
+    console.warn("Updating Product");
+    // save in the database
+    resetFields();
+  };
+  const onCreate = () => {
+    if (!validateInput()) {
       return;
     }
     console.warn("Creating Product");
     // save in the database
     resetFields();
-
-  }
+  };
+  const onDelete = () => {
+    console.warn("Deleting Product!!!!!");
+  };
+  const confirmDelete = () => {
+    // console.warn("Deleting Product");
+    Alert.alert("Confirm", "Are you sure you want to delete", [
+      { text: "Cancel" },
+      { text: "Delete", style: "destructive", onPress: onDelete },
+    ]);
+    // save in the database
+    resetFields();
+  };
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -56,18 +85,41 @@ const createProductScreen = () => {
     }
   };
   return (
-    <View className='flex-1 justify-center p-3'>
-      <Stack.Screen options={{title:"Create Product"}}/>
-      <Image className='w-3/5 aspect-square self-center' source={{uri: image ||DefaultPizzaImage}}/>
-      <Text onPress={pickImage} className='self-center font-bold my-4' style={{color:Colors.light.tint}}>Select Image</Text>
-      <Text className='text-slate-400 text-xl' >Name</Text>
-      <TextInput value={name} onChangeText={setName} placeholder='Margarita..' className='bg-white rounded-md p-2 mt-1 mb-5'/>
-      <Text className='text-slate-400 text-xl' >Price ($)</Text>
-      <TextInput value={price} onChangeText={setPrice} keyboardType='numeric' placeholder='9.99$' className='bg-white rounded-md p-2 mt-1 mb-5'/>
-      <Text className='text-red-700'>{errors}</Text>
-      <Button text="Create" onPress={onCreate}/>
+    <View className="flex-1 justify-center p-3">
+      <Stack.Screen
+        options={{ title: isUpdating ? "Updating Product" : "Create Product" }}
+      />
+      <Image
+        className="w-3/5 aspect-square self-center"
+        source={{ uri: image || DefaultPizzaImage }}
+      />
+      <Text
+        onPress={pickImage}
+        className="self-center font-bold my-4"
+        style={{ color: Colors.light.tint }}
+      >
+        Select Image
+      </Text>
+      <Text className="text-slate-400 text-xl">Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Margarita.."
+        className="bg-white rounded-md p-2 mt-1 mb-5"
+      />
+      <Text className="text-slate-400 text-xl">Price ($)</Text>
+      <TextInput
+        value={price}
+        onChangeText={setPrice}
+        keyboardType="numeric"
+        placeholder="9.99$"
+        className="bg-white rounded-md p-2 mt-1 mb-5"
+      />
+      <Text className="text-red-700">{errors}</Text>
+      <Button text={isUpdating ? "Update" : "Create"} onPress={onSubmit} />
+      {isUpdating && <Button text="Delete" onPress={confirmDelete} />}
     </View>
-  )
-}
+  );
+};
 
-export default createProductScreen
+export default createProductScreen;
